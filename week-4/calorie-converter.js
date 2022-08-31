14cbecea75a12fe116d6c8978d3541f071fe4ce1
Ace Baugh <ace.d.baugh@gmail.com>
2022-08-31 17:06:33 -0400

    Adding comments

--- week-4/calorie-converter.js
+++ week-4/calorie-converter.js
@@ -4,12 +4,16 @@
 ; Author: Professor Krasso & Ace Baugh
 ; Date: 08/31/2022
 ; Modified By: Ace Baugh
-; Description: WEB 330 Calorie App
+; Description: WEB 330 Calorie App - used Food model class to create 
+;              array of food items and has a search function to find
+;              a food item by name.
 ============================================
 */
 
+// import the FoodModel class
 import { FoodModel } from './food-model.js';
 
+// create a series of new FoodModel objects and store them in an array
 export class CalorieConverter {
 	static data = [
 		new FoodModel(1007, 'Egg', 78),
@@ -20,6 +24,7 @@ export class CalorieConverter {
 		new FoodModel(1012, 'Soda', 150),
 	];
 
+   // function that searches the array for matching strings and returns the matching objects
 	static find(string) {
 		return this.data.filter((food) => food.name.toLowerCase().includes(string.toLowerCase()));
 	}
--- week-4/food-model.js
+++ week-4/food-model.js
@@ -4,12 +4,14 @@
 ; Author: Professor Krasso & Ace Baugh
 ; Date: 08/31/2022
 ; Modified By: Ace Baugh
-; Description: WEB 330 Calorie App
+; Description: WEB 330 Calorie App - Food model class
 ============================================
 */
 
+//Creating a food model class
 export class FoodModel {
 	constructor(id, name, calories) {
+      // shorthand for this.id = id etc.;
 		Object.assign(this, { id, name, calories });
 	}
 }
