4f9ea616f1633138d432ee4092beb01ca3d64e9b
Ace Baugh <ace.d.baugh@gmail.com>
2022-09-13 17:11:30 -0400

    ground work and starting assignment 6

---
+++ week-6/required-field.js
@@ -0,0 +1,9 @@
+/* 
+============================================
+; Title: Assignment 6.2 - Future Value App - required-field.js
+; Author: Professor Krasso & Ace Baugh
+; Date: 09/13/2022
+; Modified By: Ace Baugh
+; Description: WEB 330 Future Value App
+============================================
+*/
