0358cd1b6d4e16ffdb71a44d83c3b2900b8b990a
Ace Baugh <ace.d.baugh@gmail.com>
2022-09-28 16:54:13 -0400

    Added Comments... DONE!

--- week-8/http-client.js
+++ week-8/http-client.js
@@ -1,3 +1,15 @@
+/*
+============================================
+; Title: Assignment 8.2 - What-a-book App - http-client.css
+; Author: Professor Krasso & Ace Baugh
+; Date: 09/28/2022
+; Modified By: Ace Baugh
+; Description: WEB 330 What-A-Book App
+;              This is the http-client.js file
+;              it calls to the api and returns the data
+============================================
+*/ 
+
 export class HttpClient {
 	async get(url, params = '') {
 		url = new URL(url);
